db7761e88c7696f8dcc7545abae41800aa6d43be
pragmatist1618 <pragmatist1618@gmail.com>
2021-04-20 14:29:27 +0300

    add views from viewsets

--- backend/manuscript/urls.py
+++ backend/manuscript/urls.py
@@ -1,5 +1,9 @@
-from django.urls import path
+from rest_framework import routers
+from .views import ManuscriptViewSet
+from rest_framework import routers
 
-urlpatterns = [
+from .views import ManuscriptViewSet
 
-]
+router = routers.DefaultRouter()
+router.register('', ManuscriptViewSet, basename='manuscript-api')
+urlpatterns = router.urls
--- backend/manuscript/views.py
+++ backend/manuscript/views.py
@@ -1,2 +1,12 @@
-# Create your views here.
-# TODO add get manuscript
+from rest_framework import viewsets, permissions
+
+from .models import Manuscript
+from .serializers import ManuscriptSerializer
+
+
+class ManuscriptViewSet(viewsets.ModelViewSet):
+    queryset = Manuscript.objects.all()
+    permission_classes = [
+        permissions.AllowAny,
+    ]
+    serializer_class = ManuscriptSerializer

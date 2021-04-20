0423f48a8d983ef12b3c966077442f61c9bf65c8
pragmatist1618 <pragmatist1618@gmail.com>
2021-04-20 14:45:04 +0300

    add views from viewsets

--- backend/manuscript/urls.py
+++ backend/manuscript/urls.py
@@ -1,9 +1,12 @@
 from rest_framework import routers
-from .views import ManuscriptViewSet
-from rest_framework import routers
 
 from .views import ManuscriptViewSet
 
+urlpatterns = [
+
+]
+
+# генерация url и их регистрация
 router = routers.DefaultRouter()
 router.register('', ManuscriptViewSet, basename='manuscript-api')
-urlpatterns = router.urls
+urlpatterns += router.urls
--- backend/manuscript/views.py
+++ backend/manuscript/views.py
@@ -4,9 +4,13 @@ from .models import Manuscript
 from .serializers import ManuscriptSerializer
 
 
+# используем viewsets
 class ManuscriptViewSet(viewsets.ModelViewSet):
+    # определяем набор данных
     queryset = Manuscript.objects.all()
+    # права доступа
     permission_classes = [
         permissions.AllowAny,
     ]
+    # подключаем сериалайзер
     serializer_class = ManuscriptSerializer

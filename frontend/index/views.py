da1baefa4cb5f00adb0e4b0d387fee5386f80d19
pragmatist1618 <pragmatist1618@gmail.com>
2021-04-22 14:52:18 +0300

    add simple page index

--- frontend/index/views.py
+++ frontend/index/views.py
@@ -11,15 +11,3 @@ class Home(View):
         manuscript_list_api = requests.get('http://127.0.0.1:8000/api/v1/manuscript')
         context['manuscript_list_api'] = manuscript_list_api.json()
         return render(request, "index.html", context=context)
-
-    # r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
-    # >> > r.status_code
-    # 200
-    # >> > r.headers['content-type']
-    # 'application/json; charset=utf8'
-    # >> > r.encoding
-    # 'utf-8'
-    # >> > r.text
-    # '{"type":"User"...'
-    # >> > r.json()
-    # {'private_gists': 419, 'total_private_repos': 77, ...}

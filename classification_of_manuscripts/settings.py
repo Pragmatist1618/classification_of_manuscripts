6be106cca60c945ec321eaf62796a0d1b3d8fcb2
pragmatist1618 <pragmatist1618@gmail.com>
2021-04-25 17:43:15 +0300

    added a filter on the backend by lectionary

--- classification_of_manuscripts/settings.py
+++ classification_of_manuscripts/settings.py
@@ -134,11 +134,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'backend', 'media')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static_dev'),
-)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    # os.path.join(BASE_DIR, 'static_dev'),
+]
